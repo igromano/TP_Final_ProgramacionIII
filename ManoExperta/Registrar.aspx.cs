550062d1afcda5e142a3a49f33e67814f213571d
santiagoaguirr <santiaguirre0@gmail.com>
2024-06-20 18:00:21 -0300

    agrego metodos de alta usuario y proveedor

--- ManoExperta/Registrar.aspx.cs
+++ ManoExperta/Registrar.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using dominio;
+using negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -26,13 +28,76 @@ namespace ManoExperta
             creaUsuario = true;
             creaProveedor = false;
             modalOpciones = false;
+           
+            
         }
+
+        protected void AltaUsuario(object sender, EventArgs e)
+        {
+            string nombre = TextBoxNombreUsuario.Text;
+            string email = TextBoxEmailUsuario.Text;
+            string userName = TextBoxUsuarioUsuario.Text;
+            string contrasenia = TextBoxUsuarioContrasenia.Text;
+
+            Usuario nuevoUsuario = new Usuario(userName, contrasenia);
+
+            nuevoUsuario.Email = email;
+            nuevoUsuario.UserName = userName;
+            nuevoUsuario.Nombre = nombre;
+            nuevoUsuario.RolUsuario = RolUsuario.USUARIO;
+
+
+            UsuarioNegocio usuarioNegocio = new UsuarioNegocio();
+            bool registroExitoso = usuarioNegocio.registrarUsuario(nuevoUsuario);
+
+
+            if (registroExitoso)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+
+            }
+        }
+
         protected void CrearProveedor(object sender, EventArgs e)
         {
             creaUsuario = false;
             creaProveedor = true;
-            modalOpciones = false;
+            modalOpciones = false;          
         }
+
+        void AltaProveedor(object obj, EventArgs e)
+        {
+            string nombre = TextBoxNombreProveedor.Text;
+            string email = TextBoxEmailProveedor.Text;
+            string userName = TextBoxUsuarioProveedor.Text;
+            string contrasenia = TextBoxProveedorContrasenia.Text;
+
+            Usuario nuevoProveedor = new Usuario(userName, contrasenia);
+
+            nuevoProveedor.Email = email;
+            nuevoProveedor.UserName = userName;
+            nuevoProveedor.Nombre = nombre;
+            nuevoProveedor.RolUsuario = RolUsuario.PRESTADOR;
+
+
+            UsuarioNegocio usuarioNegocio = new UsuarioNegocio();
+            bool registroExitoso = usuarioNegocio.registrarUsuario(nuevoProveedor);
+
+            
+            if (registroExitoso)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+
+            }
+            
+        }
+
 
         protected void pronvincia_SelectedIndexChanged(object sender, EventArgs e)
         {
