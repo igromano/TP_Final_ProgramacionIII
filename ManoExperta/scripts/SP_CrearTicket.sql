0ea75b9897ed69ab042e9725f3976099d5c186c6
Ezequiel Teruel <94491753+ezequieltc@users.noreply.github.com>
2024-07-14 21:54:55 -0300

    Update SP_CrearTicket.sql

--- ManoExperta/scripts/SP_CrearTicket.sql
+++ ManoExperta/scripts/SP_CrearTicket.sql
@@ -4,14 +4,15 @@ CREATE OR ALTER PROCEDURE SP_CrearTicket (
 @IdEspecialidad int,
 @Monto money = null,
 @IdEstado smallint,
-@ComentarioUsuario varchar(1000) = ''
+@ComentarioUsuario varchar(1000) = '',
+@IDUsuarioAprobacion varchar(50) = null
 ) AS
 BEGIN
 	IF @IdPrestador = '0'
 		SET @IdPrestador = null
 	IF @IdEspecialidad = 0
 		SET @IdEspecialidad = null
-	INSERT INTO Ticket(IDUsuario, IDPrestador, IDEspecialidad, Monto, IDEstado, ComentarioUsuario, FechaSolicitado) 
-	VALUES(@IdUsuario, @IdPrestador, @IdEspecialidad, @Monto, @IdEstado, @ComentarioUsuario, getdate())
+	INSERT INTO Ticket(IDUsuario, IDPrestador, IDEspecialidad, Monto, IDEstado, ComentarioUsuario, FechaSolicitado, IDUsrAprobacion) 
+	VALUES(@IdUsuario, @IdPrestador, @IdEspecialidad, @Monto, @IdEstado, @ComentarioUsuario, getdate(), @IDUsuarioAprobacion)
 
 END
